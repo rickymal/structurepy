a82c9ceb47fd3be5c34d73ea76d271b766a9b6d1
rickymal <henriquemauler@gmail.com>
2021-07-02 13:02:39 -0300

    added p5 library for front-end visual style

---
+++ p5/sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+    createCanvas(800, 800)
+}
+
+
+function draw() {
+    // if (mouseIsPressed) {
+    //     fill(0);
+    // } else {
+    //     fill(255);
+    // }
+    // ellipse(mouseX, mouseY, 80, 80)
+    
+    translation = 0
+    
+    // plano de linha do alto
+    line(0 + translation,400 + translation,40 + translation,500 + translation)
+    line(40 + translation,500 + translation,500 + translation,500 + translation)
+    line(500 + translation,500 + translation,540 + translation,400 + translation)
+    line(540 + translation,400 + translation, 0  + translation, 400 + translation)
+    
+    
+    
+    // plano de baliza
+    line(0 + translation,400 + translation,40 + translation,500 + translation)
+    line(40 + translation,500 + translation,500 + translation,500 + translation)
+    line(500 + translation,500 + translation,540 + translation,400 + translation)
+    line(540 + translation,400 + translation, 0  + translation, 400 + translation)
+    
+    
+    line(0,400,40,500)
+    line(40,500,500,500)
+    line(500,500,540,400)
+    line(540,400, 0 , 400)
+    
+    
+
+}
